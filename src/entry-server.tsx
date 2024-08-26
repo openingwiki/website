0a61f5bb647e0ef30fdb52d2aad6f4e99a851807
Asqar Arslanov <asqarslanov@gmail.com>
2024-08-26 11:27:40 +0300

    refactor: add todo comments

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -5,15 +5,17 @@ import { DEFAULT_LOCALE } from "~/lib/i18n";
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
+      // TODO: match with the current locale
       <html lang={DEFAULT_LOCALE}>
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {/* TODO: make the favicon vector */}
           <link rel="icon" href="/favicon.ico" />
           {assets}
         </head>
 
-        {/* FIXME: match with the Tailwind config. */}
+        {/* TODO: match with the Tailwind config */}
         <body id="body" class="ctp-macchiato">
           <div id="app">{children}</div>
           {scripts}
