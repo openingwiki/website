66ce199fbca168e60292697f7a1f18bfaa15b2a7
Asqar Arslanov <asqarslanov@gmail.com>
2024-08-28 19:35:08 +0300

    feat: add error boundary

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import "@fontsource-variable/wix-madefor-text";
 import { MetaProvider } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { type JSXElement } from "solid-js";
+import { ErrorBoundary, type JSXElement } from "solid-js";
 import Default from "~/layouts/Default";
 import { I18n } from "~/lib/i18n";
 import { BrandedTitle } from "~/lib/meta";
@@ -12,10 +12,15 @@ export default function App(): JSXElement {
   return (
     <I18n>
       <MetaProvider>
-        <BrandedTitle />
-        <Router root={Default}>
-          <FileRoutes />
-        </Router>
+        {/* TODO */}
+        <ErrorBoundary
+          fallback={(err) => <span>{`Error: ${err as string}`}</span>}
+        >
+          <BrandedTitle />
+          <Router root={Default}>
+            <FileRoutes />
+          </Router>
+        </ErrorBoundary>
       </MetaProvider>
     </I18n>
   );
