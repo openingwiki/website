1e5e494c7b3421ee8cd92501274d0acd6502e693
Asqar Arslanov <asqarslanov@gmail.com>
2024-08-28 19:35:29 +0300

    style: add blank line bw header and main

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -7,6 +7,7 @@ export default function Default(props: ParentProps): JSXElement {
       <div class="sticky top-0 z-10">
         <Header />
       </div>
+
       <main class="flex flex-col gap-12 p-6">{props.children}</main>
     </>
   );
