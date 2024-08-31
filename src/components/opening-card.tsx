feb3d425880a559c85b9ca69fa96bd17ec4f2927
Asqar Arslanov <asqarslanov@gmail.com>
2024-08-31 16:46:13 +0300

    feat: change behavior based on browser

--- src/components/opening-card.tsx
+++ src/components/opening-card.tsx
@@ -1,5 +1,8 @@
 import { useNavigate } from "@solidjs/router";
+import clsx from "clsx";
+import { detect } from "detect-browser";
 import { type JSXElement, type VoidProps } from "solid-js";
+import { twMerge } from "tailwind-merge";
 import Button from "~/components/button";
 import { useT } from "~/lib/i18n";
 
@@ -14,12 +17,28 @@ export default function OpeningCard(props: OpeningCardProps): JSXElement {
   const navigate = useNavigate();
   const t = useT();
 
+  const chrome = detect()?.name === "chrome";
+
+  const articleDefaultClass =
+    "group relative flex w-64 flex-col gap-2 rounded-t-2xl text-center transition hover:z-10 hover:scale-105 hover:bg-ctp-surface0/95 hover:shadow hover:backdrop-blur-sm";
+  const articleStyleChrome = "hover:bg-ctp-surface0/100";
+  const articleClass = twMerge(
+    clsx(articleDefaultClass, chrome && articleStyleChrome),
+  );
+
+  const captionDefaultClass =
+    "pointer-events-none absolute top-full flex w-full flex-col gap-1 self-center rounded-b-2xl px-4 pb-4 pt-2 transition group-hover:pointer-events-auto group-hover:bg-ctp-surface0/95 group-hover:backdrop-blur-sm";
+  const captionStyleChrome = "group-hover:bg-ctp-surface0/100";
+  const captionClass = twMerge(
+    clsx(captionDefaultClass, chrome && captionStyleChrome),
+  );
+
   const openOpening = () => {
     navigate(`/op/${props.codename}`);
   };
 
   return (
-    <article class="group relative flex w-64 flex-col gap-2 rounded-t-2xl text-center transition hover:z-10 hover:scale-105 hover:bg-ctp-surface0/95 hover:shadow hover:backdrop-blur-sm">
+    <article class={articleClass}>
       <button class="aspect-square" onClick={openOpening}>
         <img
           class="size-full rounded-2xl"
@@ -27,7 +46,7 @@ export default function OpeningCard(props: OpeningCardProps): JSXElement {
           alt={t("opening-card.thumbnail", { of: props.animeName })}
         />
       </button>
-      <div class="pointer-events-none absolute top-full flex w-full flex-col gap-1 self-center rounded-b-2xl px-4 pb-4 pt-2 transition group-hover:pointer-events-auto group-hover:bg-ctp-surface0/95 group-hover:backdrop-blur-sm">
+      <div class={captionClass}>
         <h1 class="pointer-events-auto text-lg font-medium text-ctp-green group-hover:font-semibold">
           {props.name}
         </h1>
