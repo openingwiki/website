adee2c011966b27a5b6363bb3f21504981979954
Asqar Arslanov <asqarslanov@gmail.com>
2024-08-31 18:39:13 +0300

    fix: suspense everything

--- src/app.tsx
+++ src/app.tsx
@@ -2,20 +2,22 @@ import "@fontsource-variable/wix-madefor-text";
 import { MetaProvider } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { type JSXElement } from "solid-js";
+import { Suspense, type JSXElement } from "solid-js";
 import Default from "~/layouts/default";
 import { I18nProvider } from "~/lib/i18n";
 import "./app.css";
 
 export default function App(): JSXElement {
   return (
-    <I18nProvider>
-      <MetaProvider>
-        {/* TODO */}
-        <Router root={Default}>
-          <FileRoutes />
-        </Router>
-      </MetaProvider>
-    </I18nProvider>
+    <Suspense>
+      <I18nProvider>
+        <MetaProvider>
+          {/* TODO */}
+          <Router root={Default}>
+            <FileRoutes />
+          </Router>
+        </MetaProvider>
+      </I18nProvider>
+    </Suspense>
   );
 }
