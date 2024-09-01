3c59d4b5420c988920b9a1c952c5d4a3b7185037
Asqar Arslanov <asqarslanov@gmail.com>
2024-09-01 14:57:50 +0300

    feat(api): rewrite `/op/{codename}` to use tanstack query

--- src/routes/op/[codename].tsx
+++ src/routes/op/[codename].tsx
@@ -1,64 +1,78 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
-import {
-  cache,
-  createAsync,
-  Navigate,
-  useParams,
-  type RouteDefinition,
-} from "@solidjs/router";
-import { Match, Show, Switch, type JSXElement } from "solid-js";
+import { Navigate, useParams, type RouteDefinition } from "@solidjs/router";
+import { createQuery, useQueryClient } from "@tanstack/solid-query";
+import { Show, type JSXElement } from "solid-js";
+import { QueryBoundary } from "~/components/query-boundary";
 import { getOpening } from "~/lib/api/routes/openings/[codename]";
 import { useT } from "~/lib/i18n";
 import { BrandedTitle } from "~/lib/meta";
 
-const preloadOpening = cache(async (codename: string) => {
-  "use server";
-  return await getOpening(codename);
-}, "opening");
-
 export const route: RouteDefinition = {
-  preload: ({ params }) => preloadOpening(params.codename),
+  preload: async ({ params }) => {
+    "use server";
+
+    const codename = params.codename.toLowerCase();
+
+    const queryClient = useQueryClient();
+    await queryClient.prefetchQuery({
+      queryKey: ["opening", codename],
+      queryFn: () => getOpening(codename),
+    });
+
+    // TODO
+  },
 };
 
 export default function Opening(): JSXElement {
   const params = useParams();
   const t = useT();
 
-  const opening = createAsync(() => preloadOpening(params.codename));
+  const codename = params.codename.toLowerCase();
+
+  const query = createQuery(() => ({
+    queryKey: ["opening", codename],
+    queryFn: () => getOpening(codename),
+  }));
 
   return (
-    <Switch>
-      <Match when={opening() !== undefined}>
-        <Show when={params.codename !== opening()!.codename}>
-          <Navigate href={`/op/${opening()!.codename}`} />
-        </Show>
+    <QueryBoundary
+      query={query}
+      errorFallback={() =>
+        // TODO
+        t("error.message")
+      }
+    >
+      {(opening) => (
+        <>
+          <Show when={params.codename !== opening.codename}>
+            <Navigate href={`../${opening.codename}`} />
+          </Show>
 
-        <BrandedTitle page={opening()!.name} />
+          <BrandedTitle page={opening.name} />
 
-        <iframe
-          class="aspect-video w-240 self-center rounded-xl shadow-lg"
-          title={opening()!.name}
-          src={opening()!.youtubeEmbedLink.toString()}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        />
-
-        <section class="flex flex-row items-center gap-4 self-center">
-          <img
-            class="aspect-square size-32 rounded-lg"
-            src={opening()!.thumbnailLink.toString()}
-            alt={t("opening-card.thumbnail", { of: opening()!.name })}
+          <iframe
+            class="aspect-video w-240 self-center rounded-xl shadow-lg"
+            title={opening.name}
+            src={opening.youtubeEmbedLink.toString()}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
           />
-          <div class="flex flex-col gap-2">
-            <h2 class="text-3xl font-semibold text-ctp-green">
-              {opening()!.name}
-            </h2>
-            <span class="text-ctp-subtext0">{opening()!.animeName}</span>
-          </div>
-        </section>
-      </Match>
-    </Switch>
+
+          <section class="flex flex-row items-center gap-4 self-center">
+            <img
+              class="aspect-square size-32 rounded-lg"
+              src={opening.thumbnailLink.toString()}
+              alt={t("opening-card.thumbnail", { of: opening.name })}
+            />
+            <div class="flex flex-col gap-2">
+              <h2 class="text-3xl font-semibold text-ctp-green">
+                {opening.name}
+              </h2>
+              <span class="text-ctp-subtext0">{opening.animeName}</span>
+            </div>
+          </section>
+        </>
+      )}
+    </QueryBoundary>
   );
 }
