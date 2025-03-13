e0343fdb5d35249eb9d74f69a0e6fd49fe0d0a0d
jakefish18 <jakefish.work@gmail.com>
2025-03-13 13:05:21 +0300

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
+<script scoped>
+import HeaderBar from "@/components/HeaderBar.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {HeaderBar}
+})
+
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Main</router-link>
-  </nav>
-  <router-view/>
+  <HeaderBar></HeaderBar>
+  <div class="router-container">
+    <router-view/>
+  </div>
 </template>
 
 <style>
 body {
+  margin: 0;
+  padding: 0;
   background-color: #1E1E2E;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #2c3e50;
 }
 </style>
---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="logo">
+      <span class="site-name">Opening.wiki</span>
+    </div>
+    <nav>
+      <ul>
+        <li><router-link to="/" class="link">Main</router-link></li>
+      </ul>
+    </nav>
+    <div class="profile">
+      ID: FAILED
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  font-size: 16px;
+  color: white;
+}
+
+.logo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: left;
+  gap: 10px;
+  width: 300px;
+}
+
+.profile {
+  width: 300px;
+  display: flex;
+  justify-content: right;
+}
+
+nav {
+  height: 100%;
+}
+
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  height: 100%;
+}
+
+nav ul li {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+}
+
+.site-name {
+  font-size: 24px;
+  color: white;
+  font-weight: bold;
+}
+
+.link {
+  text-decoration: none;
+  color: white;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.link:hover {
+  background-color: #1E1E2E;
+}
+
+header {
+  height: 65px;
+  background-color: #232241;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-right: 10px;
+  padding-left: 10px;
+}
+</style>
