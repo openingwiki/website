b14e4a66a23c27bae115471f0c791395f600c2af
jakefish18 <jakefish.work@gmail.com>
2025-03-13 12:52:46 +0300

    Basic template cleanup

---
+++ src/views/MainView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainView',
+});
+</script>
