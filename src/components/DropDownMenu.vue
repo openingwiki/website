a4068a845e57cf3b790977f27bac965383fd7eb5
jakefish18 <jakefish.work@gmail.com>
2025-03-15 13:00:17 +0300

    Refactoring

---
+++ src/components/DropDownMenu.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import {defineProps, ref} from 'vue'
+
+const props = defineProps<{
+  items: { label: string; action: () => void }[];
+}>();
+const isMenuVisible = ref(false);
+
+const toggleMenu = () => {
+  isMenuVisible.value = !isMenuVisible.value;
+};
+
+// eslint-disable-next-line no-undef
+defineExpose({
+  isMenuVisible,
+  toggleMenu
+})
+</script>
+
+<template>
+  <div class="container">
+    <button @click="toggleMenu" class="dropdown-btn plus-button">
+      <slot/>
+    </button>
+    <div v-if="isMenuVisible" class="dropdown-menu">
+      <button v-for="(item, index) in items" :key="index" @click="item.action" class="dropdown-item">
+        {{ item.label }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  position: relative;
+}
+
+.dropdown-menu {
+  top: 100%;
+  position: absolute;
+  background-color: #2F2D46;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 150px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  z-index: 10;
+  border-radius: 3px;
+}
+
+.dropdown-item {
+  background: none;
+  border: none;
+  width: 100%;
+  text-align: left;
+  cursor: pointer;
+  color: white;
+  padding: 10px;
+}
+
+.dropdown-item:hover {
+  background-color: #454363;
+}
+
+.dropdown-btn {
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+}
+</style>
--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,31 +1,43 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import {useUserStore} from "@/stores/user";
+import { useUserStore } from "@/stores/user";
+
+import {useTemplateRef} from "vue";
 import router from "@/router";
+import { useCookies } from "vue3-cookies";
+
+import ArrowDownSvg from "@/components/ArrowDownSvg.vue";
 import BlueButton from "@/components/BlueButton.vue";
+import DropDownMenu from "@/components/DropDownMenu.vue";
+import PlusSvg from "@/components/PlusSvg.vue";
+
 import defaultPfp from "@/assets/GojoSatoro.jpg";
-import arrowDown from "@/assets/DownArrow.svg";
-import {useCookies} from "vue3-cookies";
 
 const {cookies} = useCookies();
 const userStore = useUserStore();
+
+type dropDownMenu = InstanceType<typeof DropDownMenu>
+const profileMenuComponent = useTemplateRef<dropDownMenu>("profileMenu");
+const openingAddMenuComponent = useTemplateRef<dropDownMenu>("openingAddMenu");
 
 const redirectToAuth = (mode: 'login' | 'register') => {
   router.push({ path: `/${mode}` });
 };
 
-const isDropdownVisible = ref(false);
-
-const toggleDropdown = () => {
-  isDropdownVisible.value = !isDropdownVisible.value;
-};
-
+// Profile dropdown configuration.
 const logout = () => {
   cookies.remove("access_token");
   userStore.logout();
-
-  toggleDropdown();
+  profileMenuComponent.value.toggleMenu();
 };
+
+const profileMenuItems = [
+  {label: "Logout", action: logout}
+]
+
+// Opening add dropdown configuration.
+const openingAddMenuItems = [
+  {label: "Logout", action: logout}
+];
 </script>
 
 <template>
@@ -33,28 +45,30 @@ const logout = () => {
     <div class="logo">
       <span class="site-name">Opening.wiki</span>
     </div>
+
     <nav>
       <ul>
         <li><router-link to="/" class="link">Main</router-link></li>
       </ul>
     </nav>
-    <div v-if="userStore.isLoggedIn" class="profile">
-      <img :src="defaultPfp" class="pfp"/>
-      {{ userStore.username }}
 
-      <button @click="toggleDropdown" class="dropdown-btn">
-        <svg fill="#ffffff" height="18px" width="18px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-             viewBox="0 0 330 330" xml:space="preserve">
-        <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
-          c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
-          s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/>
-        </svg>
-      </button>
+    <div v-if="userStore.isLoggedIn" class="right-menu">
 
-      <div v-if="isDropdownVisible" class="dropdown-menu">
-        <button @click="logout" class="dropdown-item">Logout</button>
+      <drop-down-menu :items="openingAddMenuItems" class="opening-add-menu" ref="openingAddMenu">
+        <plus-svg/>
+      </drop-down-menu>
+
+      <div class="profile">
+        <img :src="defaultPfp" class="pfp"/>
+        <span>{{ userStore.username }}</span>
+
+        <drop-down-menu :items="profileMenuItems" class="profile-menu" ref="profileMenu">
+          <arrow-down-svg/>
+        </drop-down-menu>
       </div>
+
     </div>
+
     <div v-else class="auth-buttons">
       <blue-button class="auth-button" @click="redirectToAuth('login')">Log in</blue-button>
       <blue-button class="auth-button" @click="redirectToAuth('register')">Sign in</blue-button>
@@ -63,12 +77,13 @@ const logout = () => {
 </template>
 
 <style scoped>
-svg {
-  transition: fill 0.3s ease;
-}
-
-svg:hover {
-  fill: #7F91A4;
+.right-menu {
+  width: 300px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: right;
+  gap: 20px;
 }
 
 .pfp {
@@ -92,7 +107,6 @@ header {
 }
 
 .profile {
-  width: 300px;
   display: flex;
   align-items: center;
   gap: 5px;
@@ -164,42 +178,5 @@ header {
   justify-content: space-between;
   padding-right: 10px;
   padding-left: 10px;
-}
-
-
-.dropdown-btn {
-  background: none;
-  border: none;
-  cursor: pointer;
-  font-size: 18px;
-  display: flex;
-  align-items: center;
-}
-
-.dropdown-menu {
-  position: absolute;
-  top: 65px;
-  right: 0;
-  background-color: #2F2D46;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  width: 150px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  z-index: 10;
-  border-radius: 3px;
-}
-
-.dropdown-item {
-  background: none;
-  border: none;
-  width: 100%;
-  text-align: left;
-  cursor: pointer;
-  color: white;
-  padding: 10px;
-}
-
-.dropdown-item:hover {
-  background-color: #454363;
 }
 </style>
