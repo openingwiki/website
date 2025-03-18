3685c40fb27e9f48b383639a5a3d1266a6399f8b
jakefish18 <jakefish.work@gmail.com>
2025-03-18 11:25:56 +0300

    Configure v-select

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { createPinia} from "pinia";
+import VueSelect from "vue-select";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(createPinia());
+app.component("v-select", VueSelect);
 app.mount("#app");
--- src/views/OpeningAddView.vue
+++ src/views/OpeningAddView.vue
@@ -1,19 +1,42 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
-import DropBox from "@/components/DropBox.vue";
+<script setup lang="ts">
 
-export default defineComponent({
-  name: "OpeningAddView.vue",
-  components: {DropBox}
-})
+const books = [
+  { title: "Old Man's War" },
+  { title: "The Lock Artist" },
+  { title: "HTML5" },
+  { title: "Right Ho Jeeves" },
+  { title: "The Code of the Wooster" },
+  { title: "Thank You Jeeves" }
+]
 </script>
 
 <template>
-  <div>
-    <DropBox></DropBox>
-  </div>
+  <v-select :options="books" label="title" class="selecting" placeholder="Select anime"></v-select>
 </template>
 
-<style scoped>
+<style>
+@import "vue-select/dist/vue-select.css";
 
+.selecting .vs__search::placeholder,
+.selecting .vs__dropdown-toggle,
+.selecting .vs__dropdown-menu {
+  background: #2F2D46;
+  border: none;
+  color: white;
+  text-transform: lowercase;
+  font-variant: small-caps;
+}
+
+.selecting .vs__clear,
+.selecting .vs__open-indicator {
+  fill: white;
+}
+
+.selecting .vs__selected {
+  color: white;
+}
+
+.selecting {
+  min-width: 500px;
+}
 </style>
