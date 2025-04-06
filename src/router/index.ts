23288fd19cc6fbeff5bb6e227b5085595b68affc
jakefish18 <jakefish.work@gmail.com>
2025-04-06 12:14:07 +0300

    feat(site name): add site name in .env

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
 import MainView from "@/views/MainView.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
@@ -7,41 +7,41 @@ import AnimeAddView from "@/views/AnimeAddView.vue";
 import ArtistAddView from "@/views/ArtistAddView.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'main',
-    component: MainView
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: RegisterView
-  },
-  {
-    path: '/add/opening',
-    name: "add_opening",
-    component: OpeningAddView
-  },
-  {
-    path: '/add/anime',
-    name: 'add_anime',
-    component: AnimeAddView
-  },
-  {
-    path: '/add/artist',
-    name: 'add_artist',
-    component: ArtistAddView
-  }
+    {
+        path: '/',
+        name: 'main',
+        component: MainView
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginView
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: RegisterView
+    },
+    {
+        path: '/add/opening',
+        name: "add_opening",
+        component: OpeningAddView
+    },
+    {
+        path: '/add/anime',
+        name: 'add_anime',
+        component: AnimeAddView
+    },
+    {
+        path: '/add/artist',
+        name: 'add_artist',
+        component: ArtistAddView
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    pages: {
+        index: {
+            entry: 'src/main.ts',
+            title: process.env.VUE_APP_SITE_NAME
+        }
+    }
 })
